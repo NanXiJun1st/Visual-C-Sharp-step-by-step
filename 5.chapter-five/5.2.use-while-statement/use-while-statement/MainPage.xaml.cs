716889b021a4ee223a5b23a76408a9d9421406a7
NanXiJun1st <gd632631@163.com>
2023-10-19 22:41:21 +0800

    使用while语句

---
+++ 5.chapter-five/5.2.use-while-statement/use-while-statement/MainPage.xaml.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.Storage;
+using Windows.Storage.Pickers;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x804 上介绍了“空白页”项模板
+
+namespace use_while_statement
+{
+    /// <summary>
+    /// 可用于自身或导航至 Frame 内部的空白页。
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+        }
+
+        private async void OpenFileClick(object sender, RoutedEventArgs e)
+        {
+            FileOpenPicker fp = new FileOpenPicker();
+            fp.SuggestedStartLocation = PickerLocationId.DocumentsLibrary;
+            fp.ViewMode = PickerViewMode.List;
+            fp.FileTypeFilter.Add("*");
+
+            StorageFile file = await fp.PickSingleFileAsync();
+            if (file!=null)
+            {
+                FilePath.Text = file.Path;
+
+                var fileStream = await file.OpenAsync(FileAccessMode.Read);
+                var inputStream = fileStream.GetInputStreamAt(0);
+                TextReader reader = new StreamReader(inputStream.AsStreamForRead());
+                DisplayData(reader);
+            }
+        }
+
+        private void DisplayData(TextReader reader)
+        {
+            FileContent.Text = "";
+
+            string line = reader.ReadLine();
+            while (line != null)
+            {
+                FileContent.Text += line + "\n";
+                line = reader.ReadLine();
+            }
+            reader.Dispose();
+        }
+    }
+}
