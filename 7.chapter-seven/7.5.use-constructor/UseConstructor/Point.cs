8baf3783907e34feedcb2c490e97c1636890d397
NanXiJun1st <gd632631@163.com>
2023-11-13 22:32:51 +0800

    init

---
+++ 7.chapter-seven/7.5.use-constructor/UseConstructor/Point.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UseConstructor
+{
+    class Point
+    {
+        private int x, y;
+        public Point()
+        {
+            this.x = -1;
+            this.y = -1;
+            Console.WriteLine("Defaule init");
+        }
+        public Point(int x, int y)
+        {
+            this.x = x;
+            this.y = y;
+            Console.WriteLine("Assignment");
+        }
+
+        public double DistanceTo(Point other)
+        {
+            int xDiff = this.x - other.x;
+            int yDiff = this.y - other.y;
+
+            double distance = Math.Sqrt((xDiff * xDiff) + (yDiff * yDiff));
+            return distance;  
+        }
+
+        public void Deconstruct(out int x,out int y)
+        {
+            x = this.x;
+            y = this.y;
+        }
+    }
+}
---
+++ 7.chapter-seven/7.5.use-constructor/UseConstructor/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UseConstructor
+{
+    class Program
+    {
+        static void doWork()
+        {
+
+            Point origin = new Point();
+            Point bottomRight = new Point(2496, 1664);
+            double distance = origin.DistanceTo(bottomRight);
+
+            Console.WriteLine($"distance is: {distance}");
+        }
+
+        static void Main(string[] args)
+        {
+            try
+            {
+                doWork();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+
+    }
+}
