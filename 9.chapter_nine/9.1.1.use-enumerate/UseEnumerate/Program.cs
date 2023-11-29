0357fa6449ca47b014a66d265bcee600cd609423
NanXiJun1st <gd632631@163.com>
2023-11-29 21:30:55 +0800

    init

---
+++ 9.chapter_nine/9.1.1.use-enumerate/UseEnumerate/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UseEnumerate
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                doWork();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+        static void doWork()
+        {
+            Month first = Month.December;
+            Console.WriteLine(first);
+            first++;
+            Console.WriteLine(first);
+        }
+    }
+}
